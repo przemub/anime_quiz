6a36e7f9612780fb877140064c6a4778ca225ffc
Przemysław Buczkowski <przemub@przemub.pl>
2021-01-03 20:46:02 +0100

    Add Bugsnag

--- anime_quiz/settings_prod.py
+++ anime_quiz/settings_prod.py
@@ -1,3 +1,10 @@
-from .settings import *
+from .settings import *  # noqa
 
 DEBUG = False
+
+BUGSNAG = {
+    'api_key': '***REMOVED***',
+    'project_root': '/usr/src/app',
+}
+
+MIDDLEWARE += ["bugsnag.django.middleware.BugsnagMiddleware"]
