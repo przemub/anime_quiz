e03283590da8175cc212a65cf114b75b6b077ea5
Przemysław Buczkowski <przemub@przemub.pl>
2021-01-03 21:08:58 +0100

    add bugsnag handler

--- anime_quiz/settings_prod.py
+++ anime_quiz/settings_prod.py
@@ -6,5 +6,10 @@ BUGSNAG = {
     'api_key': '***REMOVED***',
     'project_root': '/usr/src/app',
 }
+LOGGING["handlers"]["bugsnag"] = {
+    'level': 'INFO',
+    'class': 'bugsnag.handlers.BugsnagHandler',
+}
+LOGGING["loggers"]["django"]["handlers"].append("bugsnag")
 
 MIDDLEWARE += ["bugsnag.django.middleware.BugsnagMiddleware"]
