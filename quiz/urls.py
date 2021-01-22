7ca6031cd00c63932b84d7ae705b84bec61bffdf
Przemysław Buczkowski <prem@prem.moe>
2021-01-22 21:51:11 +0100

    Add a robots.txt file

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from quiz.views import UserThemesView
 
-urlpatterns = [path("", UserThemesView.as_view())]
+urlpatterns = [
+    path("", UserThemesView.as_view()),
+    path("robots.txt", TemplateView.as_view(
+        template_name="quiz/robots.txt", content_type="type/plain"
+    ))
+]
