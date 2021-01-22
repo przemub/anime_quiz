e8df5f939a5ff4bcb513427952c3cf06fd50c419
Przemysław Buczkowski <prem@prem.moe>
2021-01-23 00:32:33 +0100

    Make it look good (or at least better)

--- quiz/static/quiz/main.js
+++ quiz/static/quiz/main.js
@@ -5,14 +5,15 @@ Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
 })
 
 function addUser() {
-    document.getElementById("addUserBtn").insertAdjacentHTML('beforebegin',
-        '<div class="input-group"><input type="text" class="form-control" id="user" name="user" required>\n' +
-        '                        <button class="btn" type="button"\n' +
-        '                            onclick="removeUser(this)">X</button></div>')
+    const template = document.getElementById("username-template");
+
+    document.getElementById("addUserBtn").insertAdjacentHTML(
+        "beforebegin", template.innerHTML
+    );
 }
 
 function removeUser(element) {
-    element.parentNode.parentNode.removeChild(element.parentNode)
+    element.parentNode.parentNode.removeChild(element.parentNode);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -56,38 +57,32 @@ document.addEventListener("DOMContentLoaded", function() {
     player.addEventListener('playing', () => { waiting = false; })
 });
 
-function addPlayer(element){
-    li = document.createElement("li");
-    li.className = "list-group-item"
-    li.innerHTML = '<div class="input-group">\n' +
-        '                        <button class="btn" type="button" onclick="removePlayer(this)">X</button>\n' +
-        '                        <input type="text" class="form-control user-field" id="user">\n' +
-        '                        <button class="btn" type="button" onclick="addPoint(this)">+</button>\n' +
-        '                        <button class="btn" type="button" onclick="removePoint(this)">-</button>\n' +
-        '                        <div id="points">0</div>' +
-        '                    </div>';
-    element.parentNode.getElementsByClassName("list-group")[0].appendChild(li);
+function addPlayer() {
+    const li = document.querySelector(
+        "template#results-template"
+    ).content.children[0].firstElementChild.cloneNode(true);
+    document.getElementById("results").appendChild(li);
 }
 
 function removePlayer(element){
     element.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode)
 }
 
-function clearPlayer(element){
+function clearPlayer(){
     sessionStorage.removeItem('results');
-    element.parentNode.getElementsByTagName('div')[0].innerHTML = document.getElementById('results-template').innerHTML;
+    document.getElementById("results-div").innerHTML = document.getElementById('results-template').innerHTML;
 }
 
 function addPoint(element){
-    current = element.parentNode.getElementsByTagName("div")[0].innerText;
+    let current = element.parentNode.getElementsByClassName("points")[0].innerText;
     current = parseInt(current) + 1;
-    element.parentNode.getElementsByTagName("div")[0].innerText = current;
+    element.parentNode.getElementsByClassName("points")[0].innerText = current;
 }
 
 function removePoint(element){
-    current = element.parentNode.getElementsByTagName("div")[0].innerText;
+    let current = element.parentNode.getElementsByClassName("points")[0].innerText;
     current = Math.max(parseInt(current) - 1, 0);
-    element.parentNode.getElementsByTagName("div")[0].innerText = current;
+    element.parentNode.getElementsByClassName("points")[0].innerText = current;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
