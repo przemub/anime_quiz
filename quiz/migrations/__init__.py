c2f8912961f0970875099f8d566cf17832afe37a
Przemysław Buczkowski <prem@prem.moe>
2021-01-24 20:28:13 +0100

    Add licence

---
+++ quiz/migrations/__init__.py
@@ -0,0 +1,17 @@
+#      Copyright (c) 2021 Przemysław Buczkowski
+#
+#      This file is part of Anime Quiz.
+#
+#      Anime Quiz is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU Affero General Public License as
+#      published by the Free Software Foundation, either version 3 of
+#      the License, or (at your option) any later version.
+#
+#      Anime Quiz is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU Affero General Public License for more details.
+#
+#      You should have received a copy of the GNU Affero General Public License
+#      along with Anime Quiz.  If not, see <https://www.gnu.org/licenses/>.
+
