28d7cd555b8400a154950db085f2e35ebd3ab67c
Przemysław Buczkowski <prem@prem.moe>
2022-03-07 14:18:49 +0000

    Enable manifest static files storage

--- anime_quiz/settings.py
+++ anime_quiz/settings.py
@@ -155,6 +155,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "static/"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 CACHES = {
     "default": {
